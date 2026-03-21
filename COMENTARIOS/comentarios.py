846ef2e0a5d70e308bd26f05dd6e176cdec6b0c6
CarlosF <carlos.andres.f21@hotmail.com>
2026-03-20 21:28:24 -0500

    Se crea el archivo de las diferentes formas de hacer comentarios

---
+++ COMENTARIOS/comentarios.py
@@ -0,0 +1,13 @@
+#Esta es una linea de codigo 
+print("Hola Mundo") #imprime hola mundo en pantalla
+#Este
+#es
+#un
+#comentario
+
+"""
+Este es
+un comentario
+de varias lineas
+"""
+#print("Esta linea deseo ignorarla")
